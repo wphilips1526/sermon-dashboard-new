4d45b5ac5326fab0ac8e9d6c580dfb2737c5b53e
Billy Philips <wphilips@att.net>
2025-04-15 15:43:46 -0500

    Remove all cors imports from frontend src/ directory

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+
 
 const app = express();
-app.use(cors());
+
 app.use(bodyParser.json());
 
 app.get('/files', (req, res) => {
